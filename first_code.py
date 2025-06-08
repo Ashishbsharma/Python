53bb34ae810c77a877fcc1ae4c2e736c7bfe9485
Ashish Sharma <ashishbsharma.777@gmail.com>
2025-06-07 17:07:14 -0700

    ab hua

---
+++ first_code.py
@@ -0,0 +1,5 @@
+#user input
+
+Name= input('name: ')
+age= input("Age: ")
+print("hello", Name, "hope you're having a great day you are", age, "years old")
---
+++ new.py
@@ -0,0 +1,10 @@
+# hello.py
+
+def greet(name):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    print(greet(user_name))
+
+print("This is a new change")
